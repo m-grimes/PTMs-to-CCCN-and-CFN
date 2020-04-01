dca20c94a343cfc7d3b04e2dd7ae1bbc45cac8f8
Cüneyt Gürcan Akçora <cuneyt.akcora@fulbrightmail.org>
2020-04-01 10:24:28 -0500

    first results of gene sim

--- Exp1CrossTalkNetwork.R
+++ Exp1CrossTalkNetwork.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(plyr)
+library(tidyverse)
 #Learn where data files are stored.
 source("dataConfig.R")
 
@@ -25,41 +26,78 @@ cosineSim<-function(vec1,vec2){
 
 #GeneSim computes similarity of two genes by analyzing the pathways they appear in
 # For a pair of genes, We use cosine similarity of their pathway vectors.
-geneSim<-function(file=bioProcessSimFile){
-    message(file)
-   dat<- read.csv(file,header=F,sep=",",stringsAsFactors = F)
+geneSim<-function(f=bioProcessSimFile){
+   message("Similarity file is ",f)
+   dat<- read.csv(f,header=F,sep=",",stringsAsFactors = F)
    colnames(dat)<-c("path","genelist")
+   idData <- transform(dat,id=as.numeric(factor(path)))
    #how many genes are there?
    genes<-unique(unlist(as.list(strsplit(dat$genelist, "\t"))))
    numGenes = length(genes)
    #how many pathways?
    numPathways = nrow(dat)
-   
-   vectors<-data.frame()
-   for(gene in genes){
-      vec<- (rownames(dat[dat$genelist %like% gene, ]))
-      vectors<-rbind.fill(vectors,data.frame(gene,paste(vec,collapse=" ")))
-   }
-   colnames(vectors)<-c("gene","pathways")
-   levels(droplevels(vectors$pathways))
-   simMatrix = matrix(nrow=numGenes,ncol=numPathways)
-   
-   for(i in 1:numGenes){
-      for(j in (i+1):numGenes){
-         vec1 = strsplit(vectors[i, ]," ")
-         vec2 = strsplit(vectors[j, ],"\t")
-         sim = cosineSim(vec1,vec2)
-         if(sim>0)
-         message(i," ", j," ",sim)
+   message(numGenes," genes, ",numPathways," pathways")
+   newDat<-data.frame(gene=NA,pathway=NA)
+   ps <- numeric()
+   gs <- character()
+   i<-0
+   for(row in 1:nrow(idData)){
+      p<-idData[row,"id"]
+      for(g in unlist(as.list(strsplit(idData[row,"genelist"], "\t")))){
+         i=i+1
+         ps[i] <- p
+         gs[i] <- g
       }
    }
-                    
-                    boolean<-false  
+   newDat<-data.frame(gs, ps, stringsAsFactors=FALSE)
+   simMatrix = matrix(nrow=numGenes,ncol=numGenes)
+   
+   vs<-list()
+   for(i in 1:numGenes){
+      g1<-genes[i]
+      vs[[g1]]<-newDat[newDat$gs==g1,]$ps
+   }
+   
+   l1<-character()
+   l2<-character()
+   l3<-numeric()
+   index=0
+   seen<-list()
+   for(row in 1:nrow(idData)){
+      p<-idData[row,"id"]
+      gList<-unlist(as.list(strsplit(idData[row,"genelist"], "\t")))
+      for(i in 1:length(gList)){
+         g1<-gList[i]
+         vec1 = vs[[g1]]
+         for(j in (i+1):length(gList)){
+            
+            g2<-gList[j]
+               vec2 = vs[[g2]]
+               index=index+1
+               l1[index] <- g1
+               l2[index] <- g2
+               l3[index] <- cosineSim(vec1,vec2)
+         } 
+      }
+   }
+   simDat<-data.frame(l1,l2, l3, stringsAsFactors=FALSE)
+   return(unique(simDat)) 
 }
 
 
 
 for (simFile in simFiles) {
-    message(simFile)
-    geneSim(simFile)
+   message(simFile," is being processed.")
+   tryCatch({
+      sim1<-readRDS(file = paste("results/",str_sub(simFile,start=-20),"geneSim.rds",sep=""))
+      message("Previous similarity results are loaded")
+   }, error = function(e) {
+      message("Results were not found for the file. Gene similarity will be computed now.")
+      result = geneSim(simFile)
+      saveRDS(result,file = paste("results/",str_sub(simFile,start=-20),"geneSim.rds",sep=""))
+   }, finally = {
+      
+   })
+   
+   
 }
